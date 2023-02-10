fff472e7899e6c590025b4727c0985df23b43d29
sem.dangelo <s.dangelo@opentech.com>
2023-02-10 09:57:45 +0100

    Chiusura versione 1.8.101

---
+++ AutomatIDAssets.xcframework/ios-x86_64-simulator/AutomatIDAssets.framework/Headers/AUtomatIDPlaceholderFile.h
@@ -0,0 +1,11 @@
+//
+//  OMVInnerToken.m
+//  AutomatIDAssets
+//
+//  Created by Silvio D'Angelo on 09/02/23.
+//  Copyright © 2023 opentech.com. All rights reserved.
+//
+
+@interface AUtomatIDPlaceholderFile : NSObject
+-(void) doPlaceholder;
+@end
